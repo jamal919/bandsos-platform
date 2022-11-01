bf615f8a242443355c6460fd2e1b12a741c1e5df
Jamal Khan <jamal919@gmail.com>
2022-11-02 05:26:34 +0600

    :memo: Changes gfs.py print statements to logging

--- bandsos/gfs.py
+++ bandsos/gfs.py
@@ -22,11 +22,13 @@ import os
 import shutil
 import tempfile
 import sys
+import logging
 
 def list_gfs_cycles(fdir: str, fname_pattern: str = 'gfs*.nc') -> pd.DataFrame:
     ''' 
     List available GFS forecast cycles in the `fdir` location with `fname_pattern`.
     '''
+    logging.info('In list_gfs_cycles()')
 
     fpath = glob(os.path.join(fdir, fname_pattern))
 
@@ -38,8 +40,7 @@ def list_gfs_cycles(fdir: str, fname_pattern: str = 'gfs*.nc') -> pd.DataFrame:
     cycles['cycle_id'] = [f[4:14] for f in cycles.loc[:, 'fname']] # gfs_yyyymmddhh
     cycles['start_date'] = [pd.to_datetime(f, format='%Y%m%d%H') for f in cycles.loc[:, 'cycle_id']]
     cycles = cycles.set_index('cycle_id').sort_index()
-    print('In list_gfs_cycles():')
-    print(f'\t {len(cycles)} cycles found in {fdir}')
+    logging.info(f'{len(cycles)} cycles found in {fdir}')
     return(cycles)
 
 def select_gfs_cycles(
@@ -53,6 +54,8 @@ def select_gfs_cycles(
     `forecast_length` controls how much of the forecast to be considered in the last cycle.
     `cycle_step` controls how the function search for each previous or later forecasts step by step.
     '''
+    logging.info('In select_gfs_cycles()')
+
     start_date = pd.to_datetime(start_date)
     end_date = pd.to_datetime(end_date)
     forecast_length = pd.to_timedelta(forecast_length)
@@ -69,12 +72,12 @@ def select_gfs_cycles(
     # Iteratively find the start date/cycle
     while not found_sflux_start:
         if sflux_start_date in start_dates:
-            print(f'Found {sflux_start_date} in available forecast.')
+            logging.info(f'Found {sflux_start_date} in available forecast.')
             found_sflux_start = True
         else:
-            print(f'Not found {sflux_start_date} in available forecast.')
+            logging.info(f'Not found {sflux_start_date} in available forecast.')
             sflux_start_date = sflux_start_date - cycle_step
-            print(f'Will try with {sflux_start_date} in available forecast.')
+            logging.info(f'Will try with {sflux_start_date} in available forecast.')
 
         # Check if the search is above the allowed forecast_length
         if sflux_start_date - start_date > forecast_length:
@@ -110,42 +113,41 @@ def select_gfs_cycles(
     is_selected = np.logical_and(end_dates <= sflux_end_date, start_dates >= sflux_start_date)
     selected_cycles = cycles.loc[is_selected, :].copy()
     
-    print('In select_gfs_cycles():')
-    print(f'\t   Model: {start_date} -> {end_date}')
-    print(f'\tGFS data: {sflux_start_date} -> {sflux_end_date}')
+    logging.info(f'Model: {start_date} -> {end_date}')
+    logging.info(f'GFS data: {sflux_start_date} -> {sflux_end_date}')
 
     return(selected_cycles)
 
 def _combine_gfs_cycles_temp(cycles: pd.DataFrame, fname: str, temp_dir: str, temp_name:str):
-    print(f'\tPreparing {len(cycles.index)-1} previous gfs cycles...', end='')
+    logging.info(f'Started preparing {len(cycles.index)-1} previous gfs cycles.')
     for i in range(len(cycles.index)-1):
         with xr.open_dataset(cycles.fpath[i]) as ds:
             dt = pd.to_timedelta(ds.time.diff(dim='time'))[0]
             start_time = cycles.start_date[i]
             end_time = cycles.start_date[i+1] - dt # to avoid overlapping with the next
             ds.sel(time=slice(start_time, end_time)).to_netcdf(os.path.join(temp_dir, f'{temp_name}_{i:02d}.nc'))
-    print(f'Done!')
+    logging.info(f'Done preparing previous cycles!')
 
     # The last forecast
-    print(f'\tPreparing last cycle...', end='')
+    logging.info(f'Started preparing last cycle.')
     with xr.open_dataset(cycles.fpath[-1]) as ds:
         ds.to_netcdf(
             os.path.join(temp_dir, f'{temp_name}_{len(cycles.index):02d}.nc'), 
             encoding={'time':{'units':'days since 1900-01-01'}}
             )
-    print(f'Done!')
+    logging.info(f'Done preparing last cycle')
 
     # Saving to a single file
-    print(f'\tMerging into {temp_name}_merged.nc ...', end='')
+    logging.info(f'Starts merging into {temp_name}_merged.nc')
     with xr.open_mfdataset(os.path.join(temp_dir, f'{temp_name}_*.nc')) as ds:
         ds.to_netcdf(
             os.path.join(temp_dir, f'{temp_name}_merged.nc'), 
             encoding={'time':{'units':'days since 1900-01-01'}}
         )
-    print('Done!')
+    logging.info(f'Done merging into {temp_name}_merged.nc')
 
     # Interpolate and fill na
-    print(f'\tFilling na with linear interpolation and saving to {fname}...', end='')
+    logging.info(f'Starts filling na with linear interpolation and saving to {fname}')
     with xr.open_dataset(os.path.join(temp_dir, f'{temp_name}_merged.nc')) as ds:
         ds.interpolate_na(
             dim='time'
@@ -153,7 +155,7 @@ def _combine_gfs_cycles_temp(cycles: pd.DataFrame, fname: str, temp_dir: str, te
                 fname,
                 encoding={'time':{'units':'days since 1900-01-01'}}
             )
-    print('Done!')
+    print(f'Done filling na with linear interpolation and saving to {fname}')
 
 def combine_gfs_cycles(cycles: pd.DataFrame, fname: str):
     '''
@@ -163,7 +165,7 @@ def combine_gfs_cycles(cycles: pd.DataFrame, fname: str):
 
     This method generates temporary files in system temporary folder and cleans them up upon completion or failure.
     '''
-    print('In combine_gfs_cycles():')
+    logging.info('In combine_gfs_cycles()')
     temp_name = 'comgfs'
     
     if sys.platform == 'win32':
@@ -201,7 +203,7 @@ def create_gfs_data(
     cycle_step = pd.to_timedelta(cycle_step)
     start_date = pd.to_datetime(start_date).floor(freq=cycle_step)
     end_date = pd.to_datetime(end_date).ceil(freq=cycle_step)
-    print(f'Preparing gfs data from {start_date} to {end_date}.')
+    logging.info(f'Preparing gfs data from {start_date} to {end_date}.')
 
     cycles = list_gfs_cycles(fdir=fdir, fname_pattern=fname_pattern)
     selected_cycles = select_gfs_cycles(

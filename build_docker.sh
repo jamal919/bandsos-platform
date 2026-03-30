3b68d3bbed06de6f1160f152bdb2c459b837f48c
Jamal Khan <jamal919@gmail.com>
2026-03-30 11:29:33 +0200

    :rocket: Updates docker builder
    - New builder system
    - Adds git hash and latest versioning
    - Change loading with pull

--- build_docker.sh
+++ build_docker.sh
@@ -1,4 +1,20 @@
+#!/bin/bash
+# Multiplatform: https://docs.docker.com/build/building/multi-platform/
+# docker login
 docker login
-docker run --privileged --rm docker/binfmt:a7996909642ee92942dcd6cff44b9b95f08dad64
-docker buildx build --platform linux/amd64 --push -t jamal919/bandsos:latest .
-docker buildx build --load -t jamal919/bandsos:latest .
+docker run --privileged --rm tonistiigi/binfmt --install all
+
+# builder
+docker buildx create --name mybuilder --driver docker-container --use
+docker buildx inspect --bootstrap
+
+# building
+GIT_SHA=$(git rev-parse --short HEAD)
+
+docker buildx build --platform linux/amd64 \
+      --push \
+      -t jamal919/bandsos:$GIT_SHA \
+      -t jamal919/bandsos:latest \
+      -f Dockerfile .
+
+docker pull jamal919/bandsos:latest
